7ae0f3214947f7c8844bd05250ce9fa25bd6d13f
Jeremy <jeremy.libert@luxapps.lu>
2023-11-02 20:20:42 +0100

    Added translator

---
+++ src/Interfaces/TranslatorAwareInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LibertJeremy\Symfony\Helpers\Interfaces;
+
+use LibertJeremy\Symfony\Helpers\Traits\TranslatorAwareTrait;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @see TranslatorAwareTrait
+ */
+interface TranslatorAwareInterface
+{
+    public function setTranslator(?TranslatorInterface $translator): void;
+}
---
+++ src/Traits/TranslatorAwareTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LibertJeremy\Symfony\Helpers\Traits;
+
+use LibertJeremy\Symfony\Helpers\Interfaces\TranslatorAwareInterface;
+use Symfony\Contracts\Service\Attribute\Required;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @see TranslatorAwareInterface
+ */
+trait TranslatorAwareTrait
+{
+    protected ?TranslatorInterface $translator = null;
+
+    #[Required]
+    public function setTranslator(TranslatorInterface $translator): void
+    {
+        $this->translator = $translator;
+    }
+}
