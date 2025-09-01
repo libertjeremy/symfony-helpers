d21a27b838bbf88966a2237577e6a12353331759
Jeremy <jeremy.libert@luxapps.lu>
2025-09-01 14:28:48 +0200

    Skip setups if necessary

--- src/Tests/AbstractKernelTestCase.php
+++ src/Tests/AbstractKernelTestCase.php
@@ -4,15 +4,28 @@ declare(strict_types=1);
 
 namespace LibertJeremy\Symfony\Helpers\Tests;
 
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class AbstractKernelTestCase extends KernelTestCase
 {
     protected function setUp(): void
     {
+        if (true === $this->skipSetUpBeforeBoot()) {
+            self::markTestSkipped();
+
+            return;
+        }
+
         self::bootKernel([
             'debug' => 1,
         ]);
+
+        if (true === $this->skipSetUpAfterBoot(self::getContainer())) {
+            self::markTestSkipped();
+
+            return;
+        }
 
         parent::setUp();
 
@@ -32,5 +45,15 @@ class AbstractKernelTestCase extends KernelTestCase
             $this->entityManager->close();
             $this->entityManager = null;
         }
+    }
+
+    protected function skipSetUpBeforeBoot(): bool
+    {
+        return false;
+    }
+
+    protected function skipSetUpAfterBoot(ContainerInterface $container): bool
+    {
+        return false;
     }
 }
